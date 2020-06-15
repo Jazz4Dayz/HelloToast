7c57d7f9f032bbcb79cafe99c804d213de8c510f
Jazz4Dayz <33031151+Jazz4Dayz@users.noreply.github.com>
2020-06-14 20:58:29 -0400

    added click handler to button_count

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
     public void showToast(View view) {
@@ -20,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void countUp(View view) {
+        mCount++;
 
+        if (mShowCount != null) mShowCount.setText(String.format(Locale.getDefault(), "%d", mCount));
     }
 }
