82206e167443375c7bec4f827059bfd848fb9538
Jazz4Dayz <33031151+Jazz4Dayz@users.noreply.github.com>
2020-06-15 18:46:02 -0400

    reset button now resets counter to 0 and changes color

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.hellotoast;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,12 +16,14 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private Button button_reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        button_reset = (Button) findViewById(R.id.button_reset);
     }
 
     public void showToast(View view) {
@@ -29,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
     public void countUp(View view) {
         mCount++;
 
+        if (mCount == 1) button_reset.setBackgroundColor(Color.parseColor("#FF0000"));
         if (mShowCount != null) mShowCount.setText(String.format(Locale.getDefault(), "%d", mCount));
+    }
+
+    public void resetCount(View view) {
+
+        if (mCount != 0){
+            button_reset.setBackgroundColor(Color.GRAY);
+            mShowCount.setText(String.format(Locale.getDefault(), "%d", 0));
+            mCount = 0;
+        }
     }
 }
